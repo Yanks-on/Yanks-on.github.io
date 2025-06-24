5b9b30acf321db128f6bf4a10b50b97730a2b409
Yanks-on <bouthorsyann@gmail.com>
2025-06-24 22:02:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createMeteoCard(data) {
   card.className = 'card';
 
   card.innerHTML = `
-    <h2>Météo actuelle</h2>
+    <h2>Météo actuelle à ${ville.split(',')[0]} </h2>
     <p><strong>Ville :</strong> ${ville.split(',')[0]}</p>
     <p><strong>Température :</strong> ${data.main.temp} °C</p>
     <p><strong>Ressenti :</strong> ${data.main.feels_like} °C</p>
